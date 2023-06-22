f71aa53f2a11cd1055bf408588bf318bc9762653
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-22 15:17:35 +0300

    completed task

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,12 +1,20 @@
 class BirdsController < ApplicationController
 
+
+  # Post /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+  render json: bird, status: :created
+  end
+
+
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # GET /birds/:id
+  # GET /birds/:id specific bird
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
